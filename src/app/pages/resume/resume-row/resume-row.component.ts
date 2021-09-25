dd9a8cb415397d68ad5051b8871a3579c6b54cad
Marco <marco.fox.marco@gmail.com>
2021-09-25 11:01:32 +0200

    fix date

--- src/app/pages/resume/resume-row/resume-row.component.ts
+++ src/app/pages/resume/resume-row/resume-row.component.ts
@@ -1,12 +1,11 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-resume-row',
-  templateUrl: './resume-row.component.html',
-  styleUrls: ['./resume-row.component.scss']
+  selector: "app-resume-row",
+  templateUrl: "./resume-row.component.html",
+  styleUrls: ["./resume-row.component.scss"],
 })
 export class ResumeRowComponent implements OnInit {
-
   @Input() title: string;
   @Input() subtitle?: string;
   @Input() subtitle2?: string;
@@ -23,11 +22,9 @@ export class ResumeRowComponent implements OnInit {
   @Input() single_highlight?: boolean = false;
   @Input() expanded_description?: boolean = false;
 
-  constructor() {
-  }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   divClick() {
     if (this.link) {
@@ -43,27 +40,91 @@ export class ResumeRowComponent implements OnInit {
   }
 
   private getYears(d1, d2) {
-    let y = (d2.getFullYear() - d1.getFullYear())
+    let y = d2.getFullYear() - d1.getFullYear();
     return y;
+  }
+
+  private monthToNumber(m) {
+    let month = m.split(" ")[0];
+    let n = 0;
+    switch (month) {
+      case "Jan": {
+        n = 1;
+        break;
+      }
+      case "Feb": {
+        n = 2;
+        break;
+      }
+      case "Mar": {
+        n = 3;
+        break;
+      }
+      case "Apr": {
+        n = 4;
+        break;
+      }
+      case "May": {
+        n = 5;
+        break;
+      }
+      case "Jun": {
+        n = 6;
+        break;
+      }
+      case "Jul": {
+        n = 7;
+        break;
+      }
+      case "Aug": {
+        n = 8;
+        break;
+      }
+      case "Sep": {
+        n = 9;
+        break;
+      }
+      case "Oct": {
+        n = 10;
+        break;
+      }
+      case "Nov": {
+        n = 11;
+        break;
+      }
+      case "Dec": {
+        n = 12;
+        break;
+      }
+    }
+    return n;
+  }
+
+  private dateFromStr(s) {
+    let y = s.split(" ")[1];
+    if (y == null) return new Date(s);
+    let m = this.monthToNumber(s);
+    return new Date(`${m}/1/${y}`);
   }
 
   calculatePeriod() {
     if (this.year == null) return;
 
-    let ss = this.year.split(' - ')[0]
-    let ee = this.year.split(' - ')[1]
-    let d1 = new Date(ss)
-    let d2 = (ee === "present" || ee === "now") ? new Date() : new Date(ee)
+    let ss = this.year.split(" - ")[0];
+    let ee = this.year.split(" - ")[1];
+    let d1 = this.dateFromStr(ss);
+    let d2 =
+      ee === "present" || ee === "now" ? new Date() : this.dateFromStr(ee);
+    console.log(ss, ee, d1, d2);
 
     let m = this.monthDiff(d1, d2);
     if (m != null && !isNaN(m) && m < 12) {
-      return ` (${m + 1} ${m == 0 ? 'month' : 'months'})`;
+      return ` (${m + 1} ${m == 0 ? "month" : "months"})`;
     } else {
       let y = this.getYears(d1, d2);
       if (y != null && !isNaN(y)) {
-        return ` (${y} ${y == 1 ? 'year' : 'years'})`;
+        return ` (${y} ${y == 1 ? "year" : "years"})`;
       }
     }
   }
-
 }
