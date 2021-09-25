fe597d10154c5165408f387ebe7af582fefd621a
Marco <marco.fox.marco@gmail.com>
2021-09-25 11:10:10 +0200

    fix

--- src/app/pages/resume/resume.component.ts
+++ src/app/pages/resume/resume.component.ts
@@ -1,85 +1,39 @@
-import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit} from '@angular/core';
-import {ResumeItem, SkillItem} from "../../models/resumeItem.model";
-import {ActivatedRoute} from "@angular/router";
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+} from "@angular/core";
+import { ResumeItem, SkillItem } from "../../models/resumeItem.model";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-resume',
-  templateUrl: './resume.component.html',
-  styleUrls: ['./resume.component.scss'],
+  selector: "app-resume",
+  templateUrl: "./resume.component.html",
+  styleUrls: ["./resume.component.scss"],
 })
 export class ResumeComponent implements OnInit, AfterViewInit, OnDestroy {
-
   programmingLanguages: SkillItem[] = [
-    new SkillItem(
-      "Python",
-      "icon-python.svg"
-    ),
-    new SkillItem(
-      "C#",
-      "icon-c.svg"
-    ),
-    new SkillItem(
-      "JavaScript",
-      "icon-javascript.svg"
-    ),
-    new SkillItem(
-      "TypeScript",
-      "icon-typescript.svg"
-    ),
-    new SkillItem(
-      "Java",
-      "icon-java.svg"
-    ),
-    new SkillItem(
-      "SQL",
-      "icon-mysql.svg"
-    ),
-    new SkillItem(
-      "Swift",
-      "icon-swift.svg"
-    ),
-    new SkillItem(
-      "C",
-      "icon-c.svg"
-    ),
-    new SkillItem(
-      "Scala",
-      "icon-scala.svg"
-    ),
-    new SkillItem(
-      "Ruby",
-      "icon-ruby.svg"
-    ),
-    new SkillItem(
-      "Assembly",
-      "icon-assembly.svg"
-    ),
-    new SkillItem(
-      "AppleScript",
-      "icon-apple.svg"
-    ),
-  ]
+    new SkillItem("Python", "icon-python.svg"),
+    new SkillItem("C#", "icon-c.svg"),
+    new SkillItem("JavaScript", "icon-javascript.svg"),
+    new SkillItem("TypeScript", "icon-typescript.svg"),
+    new SkillItem("Java", "icon-java.svg"),
+    new SkillItem("SQL", "icon-mysql.svg"),
+    new SkillItem("Swift", "icon-swift.svg"),
+    new SkillItem("C", "icon-c.svg"),
+    new SkillItem("Scala", "icon-scala.svg"),
+    new SkillItem("Ruby", "icon-ruby.svg"),
+    new SkillItem("Assembly", "icon-assembly.svg"),
+    new SkillItem("AppleScript", "icon-apple.svg"),
+  ];
   frameworks: SkillItem[] = [
-    new SkillItem(
-      "ASP.NET Core",
-      "icon-asp.svg"
-    ),
-    new SkillItem(
-      "EF Core",
-      "icon-asp.svg"
-    ),
-    new SkillItem(
-      "Angular",
-      "icon-angular.svg"
-    ),
-    new SkillItem(
-      "Node.js",
-      "icon-node.svg"
-    ),
-    new SkillItem(
-      "Spring",
-      "icon-spring.svg"
-    ),
+    new SkillItem("ASP.NET Core", "icon-asp.svg"),
+    new SkillItem("EF Core", "icon-asp.svg"),
+    new SkillItem("Angular", "icon-angular.svg"),
+    new SkillItem("Node.js", "icon-node.svg"),
+    new SkillItem("Spring", "icon-spring.svg"),
     // new SkillItem(
     //   "Hibernate",
     //   "icon-ibernate.png"
@@ -88,27 +42,18 @@ export class ResumeComponent implements OnInit, AfterViewInit, OnDestroy {
     //   "React.js",
     //   "icon-react.svg"
     // ),
-    new SkillItem(
-      "Bootstrap",
-      "icon-bootstrap.svg"
-    ),
-    new SkillItem(
-      "Flutter",
-      "icon-flutter.svg"
-    ),
+    new SkillItem("Bootstrap", "icon-bootstrap.svg"),
+    new SkillItem("Flutter", "icon-flutter.svg"),
     // new SkillItem(
     //   "OpenCL",
     //   "icon-opencl.png"
     // ),
-    new SkillItem(
-      "Ruby on Rails",
-      "icon-rubyonrails.svg"
-    ),
+    new SkillItem("Ruby on Rails", "icon-rubyonrails.svg"),
     // new SkillItem(
     //   "Express",
     //   "icon-express.png"
     // ),
-  ]
+  ];
   other_skills: SkillItem[] = [
     // new SkillItem(
     //   "Algorithms, Data Structures",
@@ -170,7 +115,6 @@ export class ResumeComponent implements OnInit, AfterViewInit, OnDestroy {
       "Cloud computing (Microsoft Azure, Amazon Web Services), Virtual Machines, Docker, Maven, Firebase, Pandas, Numpy, LaTeX",
       "icon-more.svg"
     ),
-
   ];
   resumeItems: ResumeItem[] = [
     // new ResumeItem(
@@ -188,7 +132,8 @@ export class ResumeComponent implements OnInit, AfterViewInit, OnDestroy {
       "https://www.youracclaim.com/badges/5e71870d-f59d-4b04-be1e-4512ec12ed83?source=linked_in_profile",
       "",
       "icon-microsoft.svg",
-      false),
+      false
+    ),
     new ResumeItem(
       "Google Campaign Manager",
       "Google",
@@ -196,7 +141,8 @@ export class ResumeComponent implements OnInit, AfterViewInit, OnDestroy {
       "https://skillshop.exceedlms.com/student/award/Wd7gcCd65frQwJwXi1DHAWAa",
       "89620252",
       "icon-google.svg",
-      false),
+      false
+    ),
     new ResumeItem(
       "Google Analytics",
       "Google",
@@ -204,7 +150,8 @@ export class ResumeComponent implements OnInit, AfterViewInit, OnDestroy {
       "https://skillshop.exceedlms.com/student/award/mj3Yw4XsxVJ3oD1ybHzwdCMm",
       "89604662",
       "icon-google.svg",
-      false),
+      false
+    ),
     new ResumeItem(
       "Fondamenti di Marketing Digitale",
       "Google Digital Technologies",
@@ -212,55 +159,8 @@ export class ResumeComponent implements OnInit, AfterViewInit, OnDestroy {
       "/assets/documents/certificates/fundamentals_of_digital_marketing.pdf",
       "XG7 Q85 4C7",
       "icon-google_light.svg",
-      false),
-    new ResumeItem(
-      "Cambridge C1 Advanced – CEFR Level C1",
-      "Cambridge School of English",
-      "Jun 2015",
-      "",
-      "",
-      "icon-language_light.svg",
-      false),
-    new ResumeItem(
-      "REST API",
-      "HackerRank",
-      "Jul 2020",
-      "https://www.hackerrank.com/certificates/579ba2349a33",
-      "579BA2349A33",
-      "icon-hackerrank.svg",
-      true),
-    new ResumeItem(
-      "Git Intermediate Techniques",
-      "LinkedIn",
-      "Aug 2020",
-      "/assets/documents/certificates/Git_Intermediate_Techniques.pdf",
-      "",
-      "icon-linkedin_light.svg",
-      true),
-    new ResumeItem(
-      "Advanced Java Programming",
-      "LinkedIn",
-      "Aug 2020",
-      "/assets/documents/certificates/Advanced_Java_Programming.pdf",
-      "",
-      "icon-linkedin_light.svg",
-      true),
-    new ResumeItem(
-      "Building Full-Stack Apps with React and Spring",
-      "LinkedIn",
-      "Aug 2020",
-      "/assets/documents/certificates/Building_Full-Stack_Apps_with_React_and_Spring.pdf",
-      "",
-      "icon-linkedin_light.svg",
-      true),
-    new ResumeItem(
-      "Ethical Hacking with JavaScript",
-      "LinkedIn",
-      "Aug 2020",
-      "/assets/documents/certificates/Ethical_Hacking_with_JavaScript.pdf",
-      "",
-      "icon-linkedin_light.svg",
-      true),
+      false
+    ),
     new ResumeItem(
       "EF Standard English Test – CEFR Level C2",
       "EF Standard English Test",
@@ -268,7 +168,61 @@ export class ResumeComponent implements OnInit, AfterViewInit, OnDestroy {
       "https://www.efset.org/cert/hrVnQr",
       "hrVnQr",
       "icon-language_light.svg",
-      false),
+      false
+    ),
+    new ResumeItem(
+      "Cambridge C1 Advanced – CEFR Level C1",
+      "Cambridge School of English",
+      "Jun 2015",
+      "",
+      "",
+      "icon-language_light.svg",
+      true
+    ),
+    new ResumeItem(
+      "REST API",
+      "HackerRank",
+      "Jul 2020",
+      "https://www.hackerrank.com/certificates/579ba2349a33",
+      "579BA2349A33",
+      "icon-hackerrank.svg",
+      true
+    ),
+    new ResumeItem(
+      "Git Intermediate Techniques",
+      "LinkedIn",
+      "Aug 2020",
+      "/assets/documents/certificates/Git_Intermediate_Techniques.pdf",
+      "",
+      "icon-linkedin_light.svg",
+      true
+    ),
+    new ResumeItem(
+      "Advanced Java Programming",
+      "LinkedIn",
+      "Aug 2020",
+      "/assets/documents/certificates/Advanced_Java_Programming.pdf",
+      "",
+      "icon-linkedin_light.svg",
+      true
+    ),
+    // new ResumeItem(
+    //   "Building Full-Stack Apps with React and Spring",
+    //   "LinkedIn",
+    //   "Aug 2020",
+    //   "/assets/documents/certificates/Building_Full-Stack_Apps_with_React_and_Spring.pdf",
+    //   "",
+    //   "icon-linkedin_light.svg",
+    //   true),
+    new ResumeItem(
+      "Ethical Hacking with JavaScript",
+      "LinkedIn",
+      "Aug 2020",
+      "/assets/documents/certificates/Ethical_Hacking_with_JavaScript.pdf",
+      "",
+      "icon-linkedin_light.svg",
+      true
+    ),
     // new ResumeItem(
     //   "Duolingo English Test",
     //   "Duolingo",
@@ -283,7 +237,8 @@ export class ResumeComponent implements OnInit, AfterViewInit, OnDestroy {
       "",
       "",
       "icon-computer_light.svg",
-      true),
+      true
+    ),
     new ResumeItem(
       "DELF - A2",
       "Conseil International d'Études Francophones (CIEF)",
@@ -291,36 +246,38 @@ export class ResumeComponent implements OnInit, AfterViewInit, OnDestroy {
       "",
       "",
       "icon-language_light.svg",
-      true),
+      true
+    ),
   ];
 
   resumeItemsVisible: ResumeItem[];
   resumeItemsHidden: ResumeItem[];
 
-  constructor(private elementRef: ElementRef,
-              private route: ActivatedRoute) {
-  }
+  constructor(private elementRef: ElementRef, private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.resumeItemsVisible = this.resumeItems.filter(x => x.hidden === false);
-    this.resumeItemsHidden = this.resumeItems.filter(x => x.hidden === true);
+    this.resumeItemsVisible = this.resumeItems.filter(
+      (x) => x.hidden === false
+    );
+    this.resumeItemsHidden = this.resumeItems.filter((x) => x.hidden === true);
   }
 
   ngAfterViewInit() {
-    document.querySelector('body').classList.add('resume');
+    document.querySelector("body").classList.add("resume");
     // this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = 'white';
     // this.elementRef.nativeElement.ownerDocument.body.style.backgroundImage = '';
     this.route.fragment.subscribe((fragment: string) => {
       if (fragment) {
         setTimeout(() => {
-          document.querySelector('#' + fragment).scrollIntoView({behavior: 'smooth'});
-        }, 100)
+          document
+            .querySelector("#" + fragment)
+            .scrollIntoView({ behavior: "smooth" });
+        }, 100);
       }
-    })
+    });
   }
 
   ngOnDestroy() {
-    document.querySelector('body').classList.remove('resume');
+    document.querySelector("body").classList.remove("resume");
   }
-
 }
