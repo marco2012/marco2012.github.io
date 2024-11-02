ea0500572276a252c95a2b4cadba18428149f637
marco <marco.fox.marco@gmail.com>
2024-11-02 13:45:33 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 // src/App.js
 // https://hygraph.com/blog/routing-in-react
 import React, { useEffect } from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import TagManager from "react-gtm-module";
 import NotFound from "./pages/NotFound";
-import { GrProjects } from "react-icons/gr";
 import Footer from "./components/Footer";
 
 function App() {
@@ -20,13 +19,13 @@ function App() {
     }, []);
     return (
         <ThemeProvider>
-            <Router>
+            <HashRouter>
                 <Routes>
                     <Route exact path="/" element={<Home />} />
                     <Route exact path="/projects" element={<Projects />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
-            </Router>
+            </HashRouter>
             <Footer />
         </ThemeProvider>
     );
